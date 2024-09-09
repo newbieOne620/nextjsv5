1f617667dedeeea6de81a706aecca5c684c97921
Lyndon Raya <lyndon.raya@nha.gov.ph>
2024-09-09 16:11:08 +0800

    update the footer

--- src/app/layout/footer.js
+++ src/app/layout/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="p-4 text-black w-screen bg-white flex justify-center items-center">
-      footer v1
+      footer version 1 Updated
     </footer>
   );
 }
