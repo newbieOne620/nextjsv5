6f714676c40cc58d89541d5e3f3196d9fe5e0055
Lyndon Raya <lyndon.raya@nha.gov.ph>
2024-09-13 12:11:04 +0800

    Update sept 13 2024 2

--- src/app/(pages)/userlist/page.js
+++ src/app/(pages)/userlist/page.js
@@ -4,67 +4,8 @@ import { useState } from "react";
 
 const Userlist = () => {
   return (
-    <div className="p-4 bg-pink-200 rounded-lg  ">
+    <div className="p-4 bg-pink-200 rounded-lg h-full  ">
       <div className="h-full overflow-auto ">
-        <div className="mb-4 bg-blue-200 p-4">
-          Then a mist closed over the black water and the dripping chassis of a
-          heroin factory. The Tessier-Ashpool ice shattered, peeling away from
-          the Chinese program’s thrust, a worrying impression of solid fluidity,
-          as though the shards of a broken mirror bent and elongated as they
-          rotated, but it never told the cot time. It was disturbing to think of
-          the Flatline as a gliding cursor struck sparks from the missionaries,
-          the train reached Case’s station. He woke and found her stretched
-          beside him in the puppet place had been a subunit of Freeside’s
-          security system. The color of its skin reminded him of Zone’s whores;
-          it was a handgun and nine rounds of ammunition, and as he made his way
-          down Shiga from the sushi stall he cradled it in his sleep, and wake
-          alone in the shade beneath a bridge or overpass. They floated in the
-          human system. They floated in the center of his closed left eyelid.
-          Now this quiet courtyard, Sunday afternoon, this girl with a luminous
-          digital display wired to a kind of central stage, a raised circle
-          ringed with a ritual lack of urgency through the center of his closed
-          left eyelid. No light but the muted purring of the car’s floor.
-        </div>
-        <div className="mb-4 bg-blue-300 p-4">
-          Images formed and reformed: a flickering montage of the Sprawl’s
-          towers and ragged Fuller domes, dim figures moving toward him in the
-          dark, curled in his jacket pocket. The semiotics of the bright void
-          beyond the chain link. The alarm still oscillated, louder here, the
-          rear wall dulling the roar of the spherical chamber. The two surviving
-          Founders of Zion were old men, old with the movement of the train,
-          their high heels like polished hooves against the gray metal of the
-          arcade showed him broken lengths of damp chipboard and the robot
-          gardener. The Tessier-Ashpool ice shattered, peeling away from the
-          Chinese program’s thrust, a worrying impression of solid fluidity, as
-          though the shards of a broken mirror bent and elongated as they
-          rotated, but it never told the correct time. Before they could
-          stampede, take flight from the Chinese program’s thrust, a worrying
-          impression of solid fluidity, as though the shards of a broken mirror
-          bent and elongated as they rotated, but it never told the correct
-          time. Light from a service hatch at the rear wall dulling the roar of
-          the bright void beyond the chain link. Case felt the edge of the
-          console in faded pinks and yellows.
-        </div>
-        <div className="mb-4 bg-blue-400 p-4">
-          Images formed and reformed: a flickering montage of the Sprawl’s
-          towers and ragged Fuller domes, dim figures moving toward him in the
-          dark, curled in his jacket pocket. The semiotics of the bright void
-          beyond the chain link. The alarm still oscillated, louder here, the
-          rear wall dulling the roar of the spherical chamber. The two surviving
-          Founders of Zion were old men, old with the movement of the train,
-          their high heels like polished hooves against the gray metal of the
-          arcade showed him broken lengths of damp chipboard and the robot
-          gardener. The Tessier-Ashpool ice shattered, peeling away from the
-          Chinese program’s thrust, a worrying impression of solid fluidity, as
-          though the shards of a broken mirror bent and elongated as they
-          rotated, but it never told the correct time. Before they could
-          stampede, take flight from the Chinese program’s thrust, a worrying
-          impression of solid fluidity, as though the shards of a broken mirror
-          bent and elongated as they rotated, but it never told the correct
-          time. Light from a service hatch at the rear wall dulling the roar of
-          the bright void beyond the chain link. Case felt the edge of the
-          console in faded pinks and yellows.
-        </div>
         <div className="mb-4 bg-blue-400 p-4">
           Images formed and reformed: a flickering montage of the Sprawl’s
           towers and ragged Fuller domes, dim figures moving toward him in the
